bbd360811a00beb15ba2e0348b5f399942a6b9e6
PutuZX <putubaguswidia@gmail.com>
2023-06-12 03:15:00 +0800

    test

--- src/app/adding_story/page.tsx
+++ src/app/adding_story/page.tsx
@@ -160,7 +160,7 @@ const AddStoryPage: React.FC = () => {
           <>
             <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-64 w-64 mb-4"></div>
             <h2 className="text-center text-gray-600">
-              The AI is Reading, please wait!
+              The bot is reading, please wait!
             </h2>
           </>
         ) : (
